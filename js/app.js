aaf332ee6072c4c9e093c37f557b5616d6f7f2fb
Tom <tsanchis@edenschool.fr>
2022-04-15 15:45:11 +0200

    nead review css js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-const backprogress = document.querySelector(".barprogress");
+// const backprogress = document.querySelector(".barprogress");
+// let temp = 0;
+// let random = Math.floor(Math.random() * (15 - 3) + 3);
+// console.log(random);
+// function progression(w) {
+//   temp = temp + w;
+//   backprogress.style.width = `${temp}%`;
+//   backprogress.style.backgroundColor = "#C84343";
+// }
 
-function progression(w) {
-  backprogress.style.width = `${w}%`;
-  backprogress.style.backgroundColor = "red";
-}
-
-setTimeout(() => progression(25), 2500);
-setTimeout(() => progression(50), 5500);
-setTimeout(() => progression(75), 7500);
-setTimeout(() => progression(100), 10500);
+// setInterval(() => progression(0.1), random * 1);
