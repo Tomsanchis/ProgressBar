b763b17e8d402bf5f8aba8c3beca10741028c8e2
Tom <tsanchis@edenschool.fr>
2022-04-15 19:51:22 +0200

    progress bar finish

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-// const backprogress = document.querySelector(".barprogress");
-// let temp = 0;
-// let random = Math.floor(Math.random() * (15 - 3) + 3);
-// console.log(random);
-// function progression(w) {
-//   temp = temp + w;
-//   backprogress.style.width = `${temp}%`;
-//   backprogress.style.backgroundColor = "#C84343";
-// }
+const backprogress = document.querySelector(".barprogress");
+let temp = 0;
+let random = Math.floor(Math.random() * (15 - 3) + 3);
+console.log(random);
+function progression(w) {
+  temp = temp + w;
+  backprogress.style.width = `${temp}%`;
+}
 
-// setInterval(() => progression(0.1), random * 1);
+setInterval(() => progression(0.1), random);
